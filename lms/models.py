bbf2ef6f044a0fda6ef1852e92aa6bb59ec4d498
Yashraj Kakkad <yashrajkakkad@gmail.com>
2021-05-15 09:21:29 +0530

    added str methods for models

--- lms/models.py
+++ lms/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
-# Create your models here.
 class Department(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
 class Course(models.Model):
     name = models.CharField(max_length=30)
     intake = models.IntegerField()    
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
@@ -15,3 +20,6 @@ class Student(models.Model):
     age = models.IntegerField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE, blank=True, null=True)
     courses = models.ManyToManyField(Course, related_name="students")
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
